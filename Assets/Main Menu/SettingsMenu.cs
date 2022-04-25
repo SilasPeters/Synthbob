09f640df8359e2a94a2e31ba2d51728e4f849fd3
SilasPeters <silaspeters03@gmail.com>
2022-04-25 16:26:09 +0200

    Almost done 3

--- Assets/Main Menu/SettingsMenu.cs
+++ Assets/Main Menu/SettingsMenu.cs
@@ -6,24 +6,30 @@ using UnityEngine.UI;
 public class SettingsMenu : MenuBehaviour
 {
 	[SerializeField] Dropdown difficultyDropdown;
+	[SerializeField] Toggle cheatsToggle;
+	[SerializeField] Toggle enableItalian;
 
 	void Start()
 	{
 		gameObject.SetActive(false);
-		
 	}
-	void OnEnable()
+
+	public void Show()
 	{
 		LoadCurrentSettings();
+		gameObject.SetActive(true);
 	}
 
 	private void LoadCurrentSettings()
 	{
-		
+		difficultyDropdown.value = Settings.Difficulty;
+		cheatsToggle.isOn = Settings.Cheats;
+		enableItalian.isOn = Settings.EnableItalian;
 	}
 
 	private void Save()
 	{
-		
+		bool succes = Settings.Save(difficultyDropdown.value, cheatsToggle.isOn, enableItalian.isOn);
+		if (!succes) Debug.LogError("Saving failed"); //todo doe aan user feedback
 	}
 }
